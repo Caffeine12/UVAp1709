b99836761baa5c71b43307ed70dd51f015e9e653
Caffeine12 <jubairku12@gmail.com>
2023-09-09 07:50:57 +0200

    UVA problem 1709 implemented but time limit exceeded-Need to improvise

--- src/Main.java
+++ src/Main.java
@@ -17,16 +17,32 @@ public class Main {
             int d=Integer.parseInt(inputArr[4]);
             int n=Integer.parseInt(inputArr[5]);
             double[] price=new double[n];
+            double highest=0;
+
+            //Calculating price from 1 up to n
 
             for(int k=1;k<=n;k++)
             {
                 price[k-1]=p*(Math.sin(a*k+b)+Math.cos(c*k+d)+2);
 
             }
+            
+            // Finding the highest value
+
+            for(int i=0;i<=n-2;i++)
+            {
+                for(int j=i+1;j<=n-1;j++)
+                {
+                    double decline = price[i]-price[j];
+                    if(decline>highest)
+                    {
+                        highest=decline;
+                    }
+                }
+            }
 
 
-
-            System.out.print(price[9]);
+            System.out.println(String.format("%.6f", highest));
 
         }
 
